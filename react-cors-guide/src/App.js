1e64025a71bd40875377efa8997187f06e0e567b
keji <756715851@qq.com>
2022-11-18 11:50:52 -0600

    first commit

---
+++ react-cors-guide/src/App.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react"
+import "./App.css"
+function App() {
+  const makeAPICall = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/cors", { mode: "cors" })
+      const data = await response.json()
+      console.log({ data })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  useEffect(() => {
+    makeAPICall()
+  }, [])
+  return (
+    <div className="App">
+      <h1>React Cors Guide</h1>
+    </div>
+  )
+}
+export default App
